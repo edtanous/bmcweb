f6a2a453bf2e1b76da93d06936fbcecc0bc24b7f
Sharad Yadav <sharady@nvidia.com>
2021-11-29 12:45:22 +0530

    Fabric: Add fabric collection to service root
    This commit adds support for fabric URI to service root.
    The implemention of fabrics is already present.
    
    Redfish validator passed.
    
    Tested:
    $curl -k -H "X-Auth-Token: $token" -H "Content-Type: application/json"
    -X GET https://${bmc}/redfish/v1/
    {
      "@odata.id": "/redfish/v1",
      "@odata.type": "#ServiceRoot.v1_5_0.ServiceRoot",
      "AccountService": {
        "@odata.id": "/redfish/v1/AccountService"
      },
      "Chassis": {
        "@odata.id": "/redfish/v1/Chassis"
      },
      "Fabrics": {
        "@odata.id": "/redfish/v1/Fabrics"
      }
    }

--- redfish-core/lib/service_root.hpp
+++ redfish-core/lib/service_root.hpp
@@ -40,6 +40,8 @@ inline void
         {"@odata.id", "/redfish/v1/AccountService"}};
     asyncResp->res.jsonValue["Chassis"] = {
         {"@odata.id", "/redfish/v1/Chassis"}};
+    asyncResp->res.jsonValue["Fabrics"] = {
+        {"@odata.id", "/redfish/v1/Fabrics"}};
     asyncResp->res.jsonValue["JsonSchemas"] = {
         {"@odata.id", "/redfish/v1/JsonSchemas"}};
     asyncResp->res.jsonValue["Managers"] = {
