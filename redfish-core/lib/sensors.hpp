a95152eca31660edad125300f8419fa6921cfa3d
Alfredo Salvarani <asalvarani@nvidia.com>
2022-11-14 12:54:58 -0800

    Add bmcweb response for Thresholds LowerFatal

--- redfish-core/lib/sensors.hpp
+++ redfish-core/lib/sensors.hpp
@@ -1047,6 +1047,9 @@ inline void objectInterfacesToJson(
         properties.emplace_back(
             "xyz.openbmc_project.Sensor.Threshold.HardShutdown",
             "HardShutdownHigh", "/Thresholds/UpperFatal/Reading"_json_pointer);
+        properties.emplace_back(
+            "xyz.openbmc_project.Sensor.Threshold.HardShutdown", "HardShutdownLow",
+            "/Thresholds/LowerFatal/Reading"_json_pointer);
     }
     else if (sensorType != "power")
     {
