1df9fc4d4731d781b8460e69d0c4e3dd5e6a9de5
Robert Papros <rpapros@nvidia.com>
2022-11-22 18:52:17 +0100

    bmcweb: Refactoring code to load configuration for CommitImage
    The previous implementation to load mapping between FW inventories and MCTP EID was in the class 'CommitImageValuesManager' in the file 'commit_image_values_manager.hpp'
    
    The class is replaced by single function getAllowableValues() which returns collection of these mappings as a vector.
    The file name is changed onto 'commit_image.hpp'
    
    Tested:
    
    Because, the part of code responsible for presenting AllowableValues is changed, the test to show CommitImage Action info is enough to verify the change
    
    1. Use GET web method to show Information about Commit Image
    
    ~ # curl 192.168.31.1:18080/redfish/v1/UpdateService/Oem/Nvidia/CommitImageActionInfo
    
    2. The result should be similar to presented below - the collection of AllowableValues should not be empty:
    
    {
      "@odata.id": "/redfish/v1/UpdateService/Oem/Nvidia/CommitImageActionInfo",
      "@odata.type": "#ActionInfo.v1_2_0.ActionInfo",
      "Id": "CommitImageActionInfo",
      "Name": "CommitImage Action Info",
      "Parameters": [
        {
          "AllowableValues": [
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_BMC_0",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_FPGA_0",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_GPU_SXM_1",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_GPU_SXM_2",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_GPU_SXM_3",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_GPU_SXM_4",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_GPU_SXM_5",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_GPU_SXM_6",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_GPU_SXM_7",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_GPU_SXM_8",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_NVSwitch_0",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_NVSwitch_1",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_NVSwitch_2",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_NVSwitch_3",
            "/redfish/v1/UpdateService/FirmwareInventory/HGX_FW_PCIeSwitch_0"
          ],
          "DataType": "StringArray",
          "Name": "Targets",
          "Required": "false"
        }
      ]
    }
    
    Fixes JIRA https://jirasw.nvidia.com/browse/DGXOPENBMC-5967
    
    Signed-off-by: Robert Papros <robert.papros@conclusive.pl>

---
+++ redfish-core/lib/commit_image.hpp
@@ -0,0 +1,87 @@
+/*
+// Copyright (c) 2022 Nvidia Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+#pragma once
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <filesystem>
+
+#include <nlohmann/json.hpp>
+
+namespace fs = std::filesystem;
+using Json = nlohmann::json;
+
+struct CommitImageValueEntry
+{
+    public:
+        std::string inventoryUri;
+        uint32_t mctpEndpointId = {};
+
+        friend bool operator== ( const CommitImageValueEntry &c1, const std::string &c2 )
+        {
+            return c1.inventoryUri == c2;
+        }
+};
+
+std::vector<CommitImageValueEntry> getAllowableValues()
+{
+    
+    static std::vector<CommitImageValueEntry> allowableValues;
+    
+    if(allowableValues.empty() == false)
+    {
+        return allowableValues;
+    }
+
+    std::string configPath = FWMCTPMAPPINGJSON;
+
+    if (!fs::exists(configPath))
+    {
+        BMCWEB_LOG_ERROR << "The file doesn't exist: " << configPath;
+        return allowableValues;
+    }
+
+    std::ifstream jsonFile(configPath);
+    auto data = Json::parse(jsonFile, nullptr, false);
+    if (data.is_discarded())
+    {
+        BMCWEB_LOG_ERROR << "Unable to parse json data " << configPath;
+        return allowableValues;
+    }
+
+    const Json emptyJson{};
+
+    auto entries = data.value("FwMctpMap", emptyJson);
+    for (const auto& element : entries.items())
+    {
+        try
+        {
+            CommitImageValueEntry allowableVal;
+
+            allowableVal.inventoryUri = element.key();
+            allowableVal.mctpEndpointId = static_cast<uint32_t>(element.value());            
+
+            allowableValues.push_back(allowableVal);
+        }
+        catch (const std::exception& e)
+        {
+            BMCWEB_LOG_ERROR << "FW MCTP EID map format error.";
+        }
+    }
+
+    return allowableValues;
+}
--- redfish-core/lib/update_service.hpp
+++ redfish-core/lib/update_service.hpp
@@ -16,7 +16,7 @@
 #pragma once
 
 #include "bmcweb_config.h"
-#include "commit_image_values_manager.hpp"
+#include "commit_image.hpp"
 #include "background_copy.hpp"
 
 #include <app.hpp>
@@ -2213,18 +2213,6 @@ inline void requestRoutesInventorySoftware(App& app)
 }
 
 /**
- * @brief Get allowable values for Commit Image action 
- * The function gets allowable values from config file
- * /usr/share/bmcweb/fw_mctp_mapping.json.
- * @returns Collection of CommitImageValueEntry.
- */
-inline std::vector<CommitImageValueEntry> getAllowableValuesForCommitImage()
-{
-    CommitImageValuesManager valuesManager;
-    return valuesManager.getAllowableValues();
-}
-
-/**
  * @brief Check whether firmware inventory is allowable 
  * The function gets allowable values from config file 
  * /usr/share/bmcweb/fw_mctp_mapping.json.
@@ -2238,7 +2226,7 @@ inline bool isInventoryAllowableValue(const std::string_view inventoryPath)
 {
     bool isAllowable = false;
 
-    std::vector<CommitImageValueEntry> allowableValues = getAllowableValuesForCommitImage();
+    std::vector<CommitImageValueEntry> allowableValues = getAllowableValues();
     std::vector<CommitImageValueEntry>::iterator it 
      = find(allowableValues.begin(), allowableValues.end(), static_cast<std::string>(inventoryPath));
 
@@ -2270,7 +2258,7 @@ inline std::pair<bool, CommitImageValueEntry> getAllowableValue(const std::strin
 {
     std::pair<bool, CommitImageValueEntry> result;
 
-    std::vector<CommitImageValueEntry> allowableValues = getAllowableValuesForCommitImage();
+    std::vector<CommitImageValueEntry> allowableValues = getAllowableValues();
     std::vector<CommitImageValueEntry>::iterator it 
      = find(allowableValues.begin(), allowableValues.end(), static_cast<std::string>(inventoryPath));
 
