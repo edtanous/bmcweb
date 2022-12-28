f0ecce7cc4280c3f4413333bb4485fc3662d322a
Aishwary Joshi <aishwaryj@nvidia.com>
2022-12-28 14:37:27 +0530

    Added 'Vendor', 'Product' and 'Description' for /redfish/v1
    This commit fixes nvbug https://nvbugs/3920884 [HGX-Vulcan] Redfish URI properties inconsistencies.
    
    Tested
    RF validator fails for /redfish/v1 with following message which is
    because TLS disabled
      1 err.SessionCollection.SessionCollection errors in /redfish/v1/
      1 failMandatoryExist errors in /redfish/v1/
    curl  -i http://localhost:28888/redfish/v1/
    {
      "@odata.id": "/redfish/v1",
      "@odata.type": "#ServiceRoot.v1_15_0.ServiceRoot",
      ...
      "Description": "Redfish Service On HGX Baseboard",
      "EventService": {
        "@odata.id": "/redfish/v1/EventService"
      },
      "Fabrics": {
        "@odata.id": "/redfish/v1/Fabrics"
      },
      "Id": "RootService",
      ....
      "Name": "Root Service",
      "Product": "NVIDIA HGX H100 8-GPU",
      ....
      "UUID": "bdeaa50d-cfab-4958-819c-fc6f14cb7998",
      "UpdateService": {
        "@odata.id": "/redfish/v1/UpdateService"
      },
      "Vendor": "NVIDIA"
    }

--- redfish-core/lib/service_root.hpp
+++ redfish-core/lib/service_root.hpp
@@ -29,6 +29,9 @@
 namespace redfish
 {
 
+constexpr std::array<const char*, 1> bmcInterfaces = {
+    "xyz.openbmc_project.Inventory.Item.BMC"};
+
 inline void
     handleServiceRootHead(App& app, const crow::Request& req,
                           const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
@@ -41,6 +44,132 @@ inline void
     asyncResp->res.addHeader(
         boost::beast::http::field::link,
         "</redfish/v1/JsonSchemas/ServiceRoot/ServiceRoot.json>; rel=describedby");
+}
+
+/**
+ * Fill out Asset information from from given D-Bus object
+ *
+ * @param[in,out]   asyncResp       Async HTTP response.
+ * @param[in]       service         D-Bus service to query.
+ * @param[in]       objpath         D-Bus object to query.
+ *                                  successfully finding object.
+ */
+inline void getBmcAssetData(std::shared_ptr<bmcweb::AsyncResp> asyncResp,
+                            const std::string& service,
+                            const std::string& objPath)
+{
+    BMCWEB_LOG_DEBUG << "Get BMC Asset Data";
+    sdbusplus::asio::getAllProperties(
+        *crow::connections::systemBus, service, objPath,
+        "xyz.openbmc_project.Inventory.Decorator.Asset",
+        [objPath, asyncResp{std::move(asyncResp)}](
+            const boost::system::error_code ec,
+            const dbus::utility::DBusPropertiesMap& properties) {
+            if (ec)
+            {
+                BMCWEB_LOG_ERROR << "DBUS response error";
+                messages::internalError(asyncResp->res);
+                return;
+            }
+
+            const std::string* name = nullptr;
+            const std::string* model = nullptr;
+            const std::string* manufacturer = nullptr;
+
+            const bool success = sdbusplus::unpackPropertiesNoThrow(
+                dbus_utils::UnpackErrorPrinter(), properties, "Name", name,
+                "Model", model, "Manufacturer", manufacturer);
+
+            if (!success)
+            {
+                BMCWEB_LOG_ERROR << "Unpack Error while fetching BMC Asset data";
+                messages::internalError(asyncResp->res);
+                return;
+            }
+
+            if (name != nullptr && !name->empty())
+            {
+                std::string description = "Redfish Service On ";
+                description += *name;
+                asyncResp->res.jsonValue["Description"] = description;
+            }
+
+            if ((model != nullptr) && !model->empty())
+            {
+                asyncResp->res.jsonValue["Product"] = *model;
+            }
+
+            if (manufacturer != nullptr)
+            {
+                asyncResp->res.jsonValue["Vendor"] = *manufacturer;
+            }
+        });
+}
+
+inline void getBMCObject(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    BMCWEB_LOG_DEBUG << "Get available BMC resources.";
+
+    // GetSubTree on all interfaces which provide info about BMC
+    crow::connections::systemBus->async_method_call(
+        [asyncResp](boost::system::error_code ec,
+                    const MapperGetSubTreeResponse& subtree) mutable {
+            if (ec)
+            {
+                BMCWEB_LOG_ERROR << "DBUS response error: " << ec;
+                messages::internalError(asyncResp->res);
+                return;
+            }
+            for (const auto& [objectPath, serviceMap] : subtree)
+            {
+                // Ignore any objects which don't end with our desired bmcid
+                if (!boost::ends_with(objectPath, PLATFORMBMCID))
+                {
+                    continue;
+                }
+
+                bool found = false;
+                // Filter out objects that don't have the BMC-specific
+                // interfaces to make sure we can return 404 on non-BMC
+                for (const auto& [serviceName, interfaceList] : serviceMap)
+                {
+                    if (std::find_first_of(
+                            interfaceList.begin(), interfaceList.end(),
+                            bmcInterfaces.begin(),
+                            bmcInterfaces.end()) != interfaceList.end())
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    continue;
+                }
+
+                for (const auto& [serviceName, interfaceList] : serviceMap)
+                {
+                    for (const auto& interface : interfaceList)
+                    {
+                        if (interface ==
+                            "xyz.openbmc_project.Inventory.Decorator.Asset")
+                        {
+                            getBmcAssetData(asyncResp, serviceName, objectPath);
+                        }
+                    }
+                }
+
+                return;
+            }
+            messages::resourceNotFound(asyncResp->res, "BMC", PLATFORMBMCID);
+        },
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTree",
+        "/xyz/openbmc_project/inventory", 0,
+        std::array<const char*, 1>{
+            "xyz.openbmc_project.Inventory.Decorator.Asset"});
 }
 
 inline void handleServiceRootGetImpl(
@@ -115,6 +244,7 @@ inline void handleServiceRootGetImpl(
     protocolFeatures["SelectQuery"] = true;
     protocolFeatures["DeepOperations"]["DeepPOST"] = false;
     protocolFeatures["DeepOperations"]["DeepPATCH"] = false;
+    getBMCObject(asyncResp);
 }
 inline void
     handleServiceRootGet(App& app, const crow::Request& req,
