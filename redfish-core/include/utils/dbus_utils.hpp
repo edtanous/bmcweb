41e0936bb97093da6138d2a11e5e111e72b04346
Ed Tanous <ed@tanous.net>
2024-02-13 14:43:34 -0800

    Create Redfish specific setProperty call
    There are currently 78 sdbusplus::asio::setProperty calls in bmcweb.
    The error handler for nearly all of them looks something like:
    
    ```
    if (ec)
    {
        const sd_bus_error* dbusError = msg.get_error();
        if ((dbusError != nullptr) &&
            (dbusError->name ==
             std::string_view(
                 "xyz.openbmc_project.Common.Error.InvalidArgument")))
        {
            BMCWEB_LOG_WARNING("DBUS response error: {}", ec);
            messages::propertyValueIncorrect(asyncResp->res, "<PropertyName>", <PropertyValue>);
            return;
        }
        messages::internalError(asyncResp->res);
        return;
    }
    messsages::sucess(asyncResp->res);
    
    ```
    
    In some cases there are more errors handled that translate to more error
    messages, but the vast majority only handle InvalidArgument.  Many of
    these, like the ones in account_service.hpp, do the error handling in a
    lambda, which causes readability problems.  It's time to make things
    consistent.
    
    This commit invents a setDbusProperty method in the redfish namespace
    that tries to handle all DBus errors in a consistent manner.  Looking
    for input on whether this will work before changing over the other 73
    calls.  Overall this is less code, fewer inline lambdas, and defaults
    that should work for MOST use cases of calling an OpenBMC daemon, and
    fall back to more generic errors when calling a "normal" dbus daemon.
    
    Change-Id: If033a1112ba516792c9386c997d090c8f9094f3a
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- redfish-core/include/utils/dbus_utils.hpp
+++ redfish-core/include/utils/dbus_utils.hpp
@@ -22,4 +22,75 @@ struct UnpackErrorPrinter
 };
 
 } // namespace dbus_utils
+
+namespace
+{
+
+void afterSetProperty(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                      const std::string& redfishPropertyName,
+                      const nlohmann::json& propertyValue,
+                      const boost::system::error_code& ec,
+                      const sdbusplus::message_t& msg)
+{
+    if (ec)
+    {
+        const sd_bus_error* dbusError = msg.get_error();
+        if (dbusError != nullptr)
+        {
+            std::string_view errorName(dbusError->name);
+
+            if (errorName == "xyz.openbmc_project.Common.Error.InvalidArgument")
+            {
+                BMCWEB_LOG_WARNING("DBUS response error: {}", ec);
+                messages::propertyValueIncorrect(
+                    asyncResp->res, redfishPropertyName, propertyValue);
+                return;
+            }
+            else if (errorName ==
+                     "xyz.openbmc_project.State.Chassis.Error.BMCNotReady")
+            {
+                BMCWEB_LOG_WARNING(
+                    "BMC not ready, operation not allowed right now");
+                messages::serviceTemporarilyUnavailable(asyncResp->res, "10");
+                return;
+            }
+            else if (errorName ==
+                     "xyz.openbmc_project.State.Host.Error.BMCNotReady")
+            {
+                BMCWEB_LOG_WARNING(
+                    "BMC not ready, operation not allowed right now");
+                messages::serviceTemporarilyUnavailable(asyncResp->res, "10");
+                return;
+            }
+        }
+        messages::internalError(asyncResp->res);
+        return;
+    }
+    asyncResp->res.result(boost::beast::http::status::no_content);
+};
+} // namespace
+
+template <typename PropertyType>
+void setDbusProperty(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                     std::string_view processName,
+                     const sdbusplus::message::object_path& path,
+                     std::string_view interface, std::string_view dbusProperty,
+                     std::string_view redfishPropertyName,
+                     const PropertyType& prop)
+{
+    std::string processNameStr(processName);
+    std::string interfaceStr(interface);
+    std::string dbusPropertyStr(dbusProperty);
+    sdbusplus::asio::setProperty(
+        *crow::connections::systemBus, processNameStr, path.str, interfaceStr,
+        dbusPropertyStr, prop,
+        [asyncResp, redfishPropertyNameStr = std::string{redfishPropertyName},
+         jsonProp = nlohmann::json(prop)](
+
+            const boost::system::error_code& ec,
+            const sdbusplus::message_t& msg) {
+        afterSetProperty(asyncResp, redfishPropertyNameStr, jsonProp, ec, msg);
+    });
+}
+
 } // namespace redfish
