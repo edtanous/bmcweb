5178d9ddf7f391b852ccd2b8a405273490a8ca3b
wthai <wthai@willie-obmc-builder.nvidia.com>
2024-04-19 05:26:30 +0000

    Resolve Conflict For Thermal
    Fixes JIRA https://upstream-sync
    Signed-off-by: wthai@nvidia.com

--- redfish-core/lib/thermal_subsystem.hpp
+++ redfish-core/lib/thermal_subsystem.hpp
@@ -44,16 +44,13 @@ inline void doThermalSubsystemCollection(
 #ifdef BMCWEB_ENABLE_HOST_OS_FEATURE
     asyncResp->res.jsonValue["Fans"]["@odata.id"] = boost::urls::format(
         "/redfish/v1/Chassis/{}/ThermalSubsystem/Fans", chassisId);
-<<<<<<< HEAD
 #endif
-=======
 
     asyncResp->res.jsonValue["ThermalMetrics"]["@odata.id"] =
         boost::urls::format(
             "/redfish/v1/Chassis/{}/ThermalSubsystem/ThermalMetrics",
             chassisId);
 
->>>>>>> master
     asyncResp->res.jsonValue["Status"]["State"] = "Enabled";
     asyncResp->res.jsonValue["Status"]["Health"] = "OK";
 #ifndef BMCWEB_DISABLE_HEALTH_ROLLUP
