ab4ccd1d515785f2cdac2d5c08790739057b7d0c
Rohit PAI <ropai@nvidia.com>
2024-07-08 11:14:16 +0530

    Fix bmcinfo in service root Fix - need to loop over all objects instead of returing on first failure tested - /redfish/v1 return Valid Product and Manudacture Name curl 192.168.31.1/redfish/v1/ {   "@odata.id": "/redfish/v1",   "@odata.type": "#ServiceRoot.v1_15_0.ServiceRoot",   "Chassis": {     "@odata.id": "/redfish/v1/Chassis"   },   "Name": "Root Service",   "Product": "P2312-A03",    "Vendor": "NVIDIA" }~ # Fixes nvbug https://nvbugspro.nvidia.com/bug/4652180 Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- redfish-core/lib/service_root.hpp
+++ redfish-core/lib/service_root.hpp
@@ -131,8 +131,7 @@ inline void getBMCObject(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
             // Ignore any objects which don't end with our desired bmcid
             if (!boost::ends_with(objectPath, PLATFORMBMCID))
             {
-                BMCWEB_LOG_ERROR("DBUS response error: {}", ec);
-                return;
+                continue;
             }
 
             bool found = false;
