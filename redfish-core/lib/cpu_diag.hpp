a547cffd338ee326d0c31d5e16e2bcaaa78dbecf
deepaky <deepaky@nvidia.com>
2024-07-23 21:25:23 -0700

    Add Redfish interface for CPU diag mode,configuration and test cases execution results.
    Fixes jira https://nvbugspro.nvidia.com/bug/4500275
    
    Testing done on Ranger board:
    
    Diag Mode Enable/Disable
    curl 'https://10.112.171.163/redfish/v1/Systems/System_0/Oem/Nvidia/ProcessorDiagCapabilities' -k -u root:0penBmc -X 'POST' -H 'Content-Type: application/json' -d '{ "ProcessorDiagCapabilities": { "DiagMode": "Enable"  } }'
    
    Diag System Config
    curl 'https://10.112.171.163/redfish/v1/Systems/System_0/Oem/Nvidia/ProcessorDiagSysConfig' -k -u root:0penBmc -X 'POST' -H 'Content-Type: application/json' -d  '{"ProcessorDiagSysConfig": [{"ConfigType": 0,"DynamicData": [1],"TestDuration": 10},{"ConfigType": 1,"DynamicData": [3,2,7,8],"TestDuration": 100}]}'
    
    Diag Tid Config
    curl 'https://10.112.171.163/redfish/v1/Systems/System_0/Oem/Nvidia/ProcessorDiagTidConfig' -k -u root:0penBmc -X 'POST' -H 'Content-Type: application/json' -d  '{"ProcessorDiagTidConfig": [{"Tid": 1,"TestDuration":10,"Loops":1000,"LogLevel":1,"DynamicDataSize":3,"DynamicData": [1,2,3]},{"Tid": 2,"TestDuration":10,"Loops":100,"LogLevel":1,"DynamicDataSize":3,"DynamicData": [7,2,3]}]}'
    
    Get api to show the result
    curl -k -u root:0penBmc --location --request GET https://10.112.171.163/redfish/v1/Systems/System_0
    
    Signed-off-by: Deepak Yadav deepaky@nvidia.com

--- redfish-core/lib/cpu_diag.hpp
+++ redfish-core/lib/cpu_diag.hpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2021-2024 NVIDIA CORPORATION &
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2024 NVIDIA CORPORATION &
  * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #pragma once
 
 #include "bmcweb_config.h"
